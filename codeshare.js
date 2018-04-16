d82a779d7db603dda85ee39fce934c9cf896f416
Steven Howard <steven@smhoward.net>
2018-04-16 11:26:09 -0400

    ready for submission

--- codeshare.js
+++ codeshare.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
       'api-key': "ce055c783ff5473b8165d9c119213281",
-      'q': userSearchInput,
-      'page': userNumberRec
+      'q': userSearchInput //, 
+      // 'page': userNumberRec << the "page" field from the API doesn't return a specific number of articles, it returns the number of pages with ten on each page. We're now limiting the number of records in the for loop below.
     });
     if(userStartYr != "") {
         url += "&begin_date=" + userStartYr + "0101";
@@ -33,13 +33,13 @@ $(document).ready(function() {
     }).done(function (result) {
       output = '';
      // Build cards for output
-      for (i = 0; i < result.response.docs.length; i++) {
+      for (i = 0; i < /*result.response.docs.length*/ userNumberRec; i++) {
         output += `
         <div class="card p-1 col-md-8 mx-auto" style = "width: 18rem;">
           <div class="card-body">
           <h5 class="card-title"> ${result.response.docs[i].snippet} </h5> 
           <h6 class="card-subtitle mb-2 text-muted">${result.response.docs[i].byline.original}</h6> 
-          <a href ="${result.response.docs[i].web_url}" class="card-link" target="_blank">${result.response.docs[i].web_url}</a>
+          <a href ="${result.response.docs[i].web_url}" class="card-link" target="_blank">Link to Article</a>
         </div> 
       </div>
       `
@@ -47,10 +47,10 @@ $(document).ready(function() {
       // Update UI
       document.getElementById('articles').innerHTML = output;
       // If we want input fields cleared automatically upon submission of search
-      // $('#search-term').val('');
-      // $('#number-records').val('');
-      // $('#start-yr').val('');
-      // $('#end-yr').val('');
+      $('#search-term').val('');
+      $('#number-records').val('');
+      $('#start-yr').val('');
+      $('#end-yr').val('');
 
       // Clear Results Button
       $('#clear-results').on('click', function(e){
